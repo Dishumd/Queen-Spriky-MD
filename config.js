d2b092a0d8726c7448c2e3f815aff7850d52be4a
Dishumd <dishumd984@gmail.com>
2024-11-25 11:22:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || '', // Enter Your Session ID
-    MONGODB: process.env.MONGODB || '',// Enter Your MongoDB URL
-    OWNER_NUMBER: process.env.OWNER_NUMBER || '',    // Enter Your Owner Number
+    SESSION_ID: process.env.SESSION_ID || 'OJcVkCbZ#5C_9G7zxqepWWPz9BSjejVm1-YhD0rVaAC05SI0sVVA', // Enter Your Session ID
+    MONGODB: process.env.MONGODB || 'mongodb+srv://cpro95528:shashika2005@mrhansamala.fuzm7.mongodb.net/?retryWrites=true&w=majority&appName=mrhansamala',// Enter Your MongoDB URL
+    OWNER_NUMBER: process.env.OWNER_NUMBER || '94751629685',    // Enter Your Owner Number
 };
